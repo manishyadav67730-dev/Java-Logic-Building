cd70337dd231500ca765f8b850772b5a887c4582
manishyadav67730-dev <manishyadav67730@gmail.com>
2026-06-14 10:51:14 +0530

    Add files via upload
    printing left side of pyramid by taking input the height

---
+++ Patterns/lefthalfpyramid.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class lefthalfpyramid {
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System .in);
+    System.out.println("enter height of the half pyramid");
+    int m = sc.nextInt();
+
+    for(int i = 1;  i<=m;  i++){
+        for(int j=1; j<=m-i; j++){
+            System.out.print(" ");
+        }
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+    
+    System.out.println();
+    }
+ }   
+}
+ 
